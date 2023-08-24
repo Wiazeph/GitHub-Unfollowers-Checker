279e6390096e8223f188ccfc0a7089021a67966b
Emre Erden <emreerden@pm.me>
2023-08-25 00:04:45 +0300

    create Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-function App() {
+import { Header } from "./components/Header";
 
+function App() {
   return (
     <>
-      <div className="text-3xl font-bold underline">GitHub Unfollower Checker</div>
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+  return (
+    <header className="Header">
+      <div className="container mx-auto py-6 sm:py-8 md:py-10 lg:py-12">
+        <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-center">
+          GitHub Unfollower Checker
+        </div>
+      </div>
+    </header>
+  );
+};
