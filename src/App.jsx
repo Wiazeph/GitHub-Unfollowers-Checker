8085d74233620a10728c2f4b222707c88e1c80c5
Emre Erden <emreerden@pm.me>
2023-08-25 00:12:32 +0300

    create Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
