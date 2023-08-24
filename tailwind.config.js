47358bb9a8f03faece3e6114a0d65b76e1e547c7
Emre Erden <emreerden@pm.me>
2023-08-25 00:22:59 +0300

    style: edit container screens sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        screens: {
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
