532733b5dff477d85595b571f38bcca277d05c69
Emre Erden <emreerden@pm.me>
2023-08-25 16:49:29 +0300

    create Main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header";
+import { Main } from "./components/main/Main";
 import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
       <Footer />
     </>
   );
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,21 @@
+export const Main = () => {
+  return (
+    <main className="Main">
+      <div className="container mx-auto flex flex-col gap-8 sm:gap-10 pt-2 sm:pt-0 overflow-y-scroll">
+        <div>input input button</div>
+        <div>list</div>
+        <div>list</div>
+        <div>list</div>
+        <div>list</div>
+        <div>list</div>
+        <div>list</div>
+        <div>list</div>
+        <div>list</div>
+        <div>list</div>
+        <div>list</div>
+        <div>list</div>
+        <div>list</div>
+      </div>
+    </main>
+  );
+};
