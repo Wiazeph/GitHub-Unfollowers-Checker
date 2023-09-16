6cd8868bcc3cfcd8596e964aac94b7b4762e65c8
Emre Erden <emreerden@pm.me>
2023-09-16 17:17:52 +0300

    edit title

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <footer className="Footer">
       <ul className="container mx-auto bg-neutral-800 rounded-xl py-4 flex items-center gap-5 sm:gap-10 select-none">
-        <li className="w-full text-right">GitHub Unfollower Checker</li>
+        <li className="w-full text-right">GitHub Unfollowers Checker</li>
         <li className="border border-gray-50 h-fit"></li>
         <li className="w-full flex justify-start">
           <a
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export const Header = () => {
   return (
     <header className="Header">
       <div className="container mx-auto py-4 sm:py-6 md:py-8 text-3xl sm:text-4xl md:text-5xl font-bold text-center">
-        GitHub Unfollower Checker
+        GitHub Unfollowers Checker
       </div>
     </header>
   );
