f7660864621231e24e5057fc34f5413924bc39cf
Emre Erden <emreerden@pm.me>
2024-02-17 03:28:49 +0300

    add card loader animation

--- src/components/main/UnfollowersList.jsx
+++ src/components/main/UnfollowersList.jsx
@@ -14,30 +14,40 @@ export const UnfollowersList = () => {
           <div className="mt-6 text-center text-xl">{`${unfollowers.length} Users`}</div>
 
           <ul className="UnfollowersList no-scrollbar overflow-y-scroll flex flex-wrap justify-center sm:justify-start">
-            {unfollowers.map((unfollower) => (
-              <li
-                key={unfollower.id}
-                className="w-full sm:w-1/2 md:w-1/3 max-w-xs sm:max-w-none"
-              >
-                <a
-                  href={unfollower.html_url}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="p-4 rounded-md flex gap-x-4 items-center hover:bg-neutral-800 transition-colors"
-                >
-                  <img
-                    src={unfollower.avatar_url}
-                    alt="avatar"
-                    className="object-cover rounded-full w-10 h-10"
-                  />
+            {unfollowers.length === 0
+              ? [...Array(9)].map((_, index) => (
+                  <UnfollowersList.Loader key={index} />
+                ))
+              : unfollowers.map((unfollower) => (
+                  <li
+                    key={unfollower.id}
+                    className="w-full sm:w-1/2 md:w-1/3 max-w-xs sm:max-w-none"
+                  >
+                    <a
+                      href={unfollower.html_url}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="h-16 px-3 rounded-md flex gap-x-4 items-center hover:bg-neutral-800 transition-colors"
+                    >
+                      <img
+                        src={unfollower.avatar_url}
+                        alt="avatar"
+                        className="object-cover rounded-full w-10 h-10"
+                      />
 
-                  <div className="font-semibold">{unfollower.login}</div>
-                </a>
-              </li>
-            ))}
+                      <div className="font-semibold">{unfollower.login}</div>
+                    </a>
+                  </li>
+                ))}
           </ul>
         </>
       )}
     </>
+  )
+}
+
+UnfollowersList.Loader = function UnfollowersListLoader() {
+  return (
+    <div className="w-full sm:w-1/2 md:w-1/3 max-w-xs sm:max-w-none h-16 animate-pulse rounded-md" />
   )
 }
